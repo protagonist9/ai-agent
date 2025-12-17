7b846e635e1782f3d96e4b93fa61a18d1612e46d
protagonist9 <bozgan.tarik@gmail.com>
2025-12-18 01:10:22 +0300

    enable LLM tools for reading, writing, listing, and running files

--- functions/write_file_content.py
+++ functions/write_file_content.py
@@ -1,4 +1,7 @@
 import os
+from google.genai import types
+
+
 def write_file(working_directory, file_path, content):
     full_path = os.path.join(working_directory, file_path)
     abs_working = os.path.abspath(working_directory)
@@ -10,3 +13,22 @@ def write_file(working_directory, file_path, content):
     with open(full_path, "w") as f:
         f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes or overwrites a file with the given content within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The text content to write into the file.",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
